c6f4fb53e380a5f053afd08a5454b7043b8a4515
Alex Straw <as17163@bristol.ac.uk>
2021-04-14 02:29:34 +0000

    working with final data array printing

--- feature_sorting/main_feature_battery.py
+++ feature_sorting/main_feature_battery.py
@@ -25,11 +25,11 @@ def initialise_phone_objects():
     phone_2_unlit = Phone(2, 22, 56, 0.05, 100000, False)
     phone_2_lit = Phone(2, 22, 56, 0.05, 100000, True)
 
-    phone_3_unlit = Phone(3, 53, 87, 0.02, 500000, False)
-    phone_3_lit = Phone(3, 53, 87, 0.02, 500000, True)
+    phone_3_unlit = Phone(3, 28, 53, 0.02, 500000, False)
+    phone_3_lit = Phone(3, 46, 90, 0.02, 500000, True)
 
-    phone_4_unlit = Phone(4, 65, 153, 0.05, 500000, False)
-    phone_4_lit = Phone(4, 65, 153, 0.05, 500000, True)
+    phone_4_unlit = Phone(4, 57, 90, 0.05, 500000, False)
+    phone_4_lit = Phone(4, 56, 179, 0.05, 500000, True)
 
     all_phones = [Phone_1_unlit, phone_1_lit, phone_2_unlit, phone_2_lit, phone_3_unlit, phone_3_lit, phone_4_unlit,
                   phone_4_lit]
@@ -69,28 +69,53 @@ def get_actual_battery_centre(worksheet, current_phone, photo_number):
     return ([x, y])
 
 
-def testing_loop(current_path, all_phones, worksheet):
+def testing_loop(current_path, all_phones, worksheet, results_data):
     for photo_block in range(0, 8):  # 5 photos in each block, 2 phone blocks for each phone, and 8 phones in total
         current_phone = all_phones[photo_block]
 
         thresh_lower = current_phone.thresh_lower
         thresh_upper = current_phone.thresh_upper
 
-
         for photo_number in range(0, 5):
             actual_battery_centre = get_actual_battery_centre(worksheet, current_phone, photo_number)
             image = get_phone_image(current_path, current_phone, photo_number)
 
-            feature_battery_automatic.find_features(image, thresh_lower, thresh_upper)
+            battery_outline, estimated_centre = feature_battery_automatic.find_features(image, thresh_lower,
+                                                                                        thresh_upper)
+
+            if photo_block % 2 == 0:  # naturally lit photos
+                #results_data[photo_number, (current_phone.number * 2 - 2)] = estimated_centre[0]
+                #results_data[photo_number, (current_phone.number * 2 - 1)] = estimated_centre[1]
+                results_data[(current_phone.number * 2 - 2), photo_number] = estimated_centre[0]
+                results_data[(current_phone.number * 2 - 1), photo_number] = estimated_centre[1]
+            else:  # diffuse lit photos
+                results_data[(current_phone.number * 2 - 2), photo_number+5] = estimated_centre[0]
+                results_data[(current_phone.number * 2 - 1), photo_number+5] = estimated_centre[1]
+
+    return (results_data)
+
+
+def setup_results_dataframe(results):
+    results_df = pandas.DataFrame(results,
+                                  columns=['phone 1 x', 'phone 1 y', 'phone 2 x', 'phone 2 y', 'phone 3 x', 'phone 3 y',
+                                           'phone 4 x', 'phone 2 4 y', ])
+    return results_df
+
 
 def main():
+    results_data = np.zeros(shape=(8, 10))  # set all results to 0
     current_path = os.path.dirname(__file__)
     workbook_path = (current_path + '\Actual_Battery_Centres.xlsx')
     worksheet = pandas.read_excel(workbook_path, sheet_name='Sheet1')
 
     all_phones = initialise_phone_objects()
 
-    testing_loop(current_path, all_phones, worksheet)
+    results = testing_loop(current_path, all_phones, worksheet, results_data)
+
+    results = np.transpose(results)
+
+    results_df = setup_results_dataframe(results)
+    print(results_df)
 
 
 if __name__ == '__main__':
